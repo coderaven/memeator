13ea22dc9f4afd94c3dbca72990aaffae4329673
Raven Duran <raven@webnified.com>
2014-08-14 13:04:19 +0800

    Now able to drag and drop then change image

--- src/memeator/Memeator.java
+++ src/memeator/Memeator.java
@@ -6,7 +6,9 @@
 
 package memeator;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util;
 import java.awt.BorderLayout;
+import java.awt.Graphics;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
@@ -15,10 +17,14 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 /**
@@ -27,25 +33,26 @@ import javax.swing.JTextArea;
  */
 public class Memeator extends JFrame implements DropTargetListener {
     private JTextArea jt;
+    private ImagePanel iPanel;
     
     Memeator(){
         setTitle("Drag and Drop Example");
-        setSize(400,400);
         setVisible(true);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         // Create JTextArea
-        jt = new JTextArea();
-        add(jt);
+        iPanel = new ImagePanel();
+        add(iPanel);
         
         enableDragAndDrop();
         setLocationRelativeTo(null);
+        setSize(600, 400);
         show();
     }
     
     private void enableDragAndDrop(){
-        DropTarget target = new DropTarget(jt,this);
+        DropTarget target = new DropTarget(this,this);
     }
     
     
@@ -84,10 +91,50 @@ public class Memeator extends JFrame implements DropTargetListener {
             
             // Now get the first file from the list
             File file=(File)list.get(0);
-            jt.read(new FileReader(file),null);
+            //jt.read(new FileReader(file),null);
+            //jt.setText(file.getPath());
+            //JOptionPane.showMessageDialog(null, "The file location is " + file.getPath());
+            this.iPanel.setImage(file.getPath());
         } catch (UnsupportedFlavorException | IOException ex){
             
         }
     }
     
+}
+
+class ImagePanel extends JPanel {
+    private BufferedImage image;
+
+    public ImagePanel(String imageFile) {
+       try {                
+          image = ImageIO.read(new File(imageFile));
+       } catch (IOException ex) {
+            // handle exception...
+       }
+    }
+    
+    public ImagePanel(){
+        try {                
+            image = ImageIO.read(new File("placeholder.jpeg"));
+         } catch (IOException ex) {
+              // handle exception...
+         }
+    }
+    
+    public void setImage(String imageFile){
+        try {                
+            image = ImageIO.read(new File(imageFile));
+         } catch (IOException ex) {
+              // handle exception...
+         }
+        
+        this.repaint();
+        this.revalidate();
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters            
+    }
 }
