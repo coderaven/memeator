d692a3a0877e1d6c72cd1ee6f2be33932b60b7f4
Raven Duran <raven@webnified.com>
2014-08-15 13:25:02 +0800

    Now able to drag and drop then change image

--- src/memeator/Memeator.java
+++ src/memeator/Memeator.java
@@ -6,9 +6,14 @@
 
 package memeator;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
@@ -19,11 +24,9 @@ import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -36,18 +39,19 @@ public class Memeator extends JFrame implements DropTargetListener {
     private ImagePanel iPanel;
     
     Memeator(){
-        setTitle("Drag and Drop Example");
+        setTitle("MEME-ATOR");
         setVisible(true);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         // Create JTextArea
         iPanel = new ImagePanel();
+        iPanel.setMaximumSize(new Dimension(800, 800));
         add(iPanel);
         
         enableDragAndDrop();
         setLocationRelativeTo(null);
-        setSize(600, 400);
+        setSize(800, 800);
         show();
     }
     
@@ -132,9 +136,72 @@ class ImagePanel extends JPanel {
         this.revalidate();
     }
     
+    public static BufferedImage toBufferedImage(Image img){
+        if (img instanceof BufferedImage){
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters            
+        //g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters ;
+        image = textOverlayImage(toBufferedImage(image.getScaledInstance(500, -1, Image.SCALE_SMOOTH)), "HE HELLO HELLO HELLO");
+        g.drawImage(image, 0, 0, null);
+    }
+    
+    private BufferedImage textOverlayImage(BufferedImage old, String text){
+        int w = old.getWidth();
+        int h = old.getHeight();
+        
+        BufferedImage img = new BufferedImage(
+            w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        
+        
+        g2d.drawImage(old, 0, 0, null);
+        
+        g2d.setFont(new Font("Arial", Font.BOLD, 40));
+        FontMetrics fm = g2d.getFontMetrics();
+        
+        int x = 0;
+        int y = fm.getHeight();
+        
+        int stringLen = (int)  g2d.getFontMetrics().getStringBounds(text, g2d).getWidth();  
+        int start = 250 - stringLen/2;
+
+        g2d.setColor(Color.black);
+        g2d.drawString(text, start + ShiftWest(x, 1), ShiftNorth(y, 1));
+        g2d.drawString(text, start + ShiftWest(x, 1), ShiftSouth(y, 1));
+        g2d.drawString(text, start + ShiftEast(x, 1), ShiftNorth(y, 1));
+        g2d.drawString(text, start + ShiftEast(x, 1), ShiftSouth(y, 1));
+        g2d.setColor(Color.white);
+        g2d.drawString(text, start + x, y);
+        g2d.dispose();
+        return img;
+    }
+    
+    int ShiftNorth(int p, int distance) {
+        return (p - distance);
+    }
+    int ShiftSouth(int p, int distance) {
+        return (p + distance);
+    }
+    int ShiftEast(int p, int distance) {
+        return (p + distance);
+    }
+    int ShiftWest(int p, int distance) {
+        return (p - distance);
     }
 }
